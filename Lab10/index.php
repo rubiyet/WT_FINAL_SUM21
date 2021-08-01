a3a175b43b5f31f12247a32416c47630350be074
rubiyet <rubiyetfardous@gmail.com>
2021-08-01 15:31:46 +0600

    Lab10

---
+++ Lab10/index.php
@@ -0,0 +1,18 @@
+<script>
+	function get(id){
+		return document.getElementById(id);
+	}
+	
+	function loadDoc(){
+		var xhr = new XMLHttpRequest();
+		xhr.open("GET","profile.php",true);
+		xhr.onreadystatechange=function(){
+			if(this.readyState == 4 && this.status == 200){
+				get("demo").innerHTML = this.responseText;
+			}
+		};
+		xhr.send();
+	}
+</script>
+<button onclick="loadDoc()">Click me</button>
+<div id="demo"></div>
---
+++ Lab10/profile.php
@@ -0,0 +1,12 @@
+<html>
+		<head></head>
+		<?php $name = "Rubiyet";?>
+	<body>
+		<h1 align="center">MY PROFILE</h1>
+		<table align="center">
+			<tr>
+				<td><?php echo $name;?></td>
+			</tr>
+		</table>
+	</body>
+</html>
